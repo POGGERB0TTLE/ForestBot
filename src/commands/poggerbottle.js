3105195437546cb820f2e03a93c08664425e07a4
POGGERB0TTLE <89946902+POGGERB0TTLE@users.noreply.github.com>
2021-09-01 16:10:24 -0700

    Update poggerbottle.js

--- src/commands/poggerbottle.js
+++ src/commands/poggerbottle.js
@@ -1,1 +1,24 @@
 //Poggerbottle's command 
+```js
+module.exports = {
+    commands: ['poggerbottle'],
+    minArgs: 0,
+    maxArgs: 0,
+    callback: async (username, message, arguments, text, bot) => {
+        await sleep(600);
+
+        var word1 = [
+                  "Very Poggers guy.",
+                  "Kinda Pog, lul",
+                  "Bussin respectfully ", 
+                  "Proud Forestbot Suporter",
+                  "Rightful owner of the word Pog"
+              ];
+
+        var r = Math.floor(Math.random() * word1.length);
+        bot.chat(word1[r])
+    },
+}
+```
+this is your command, just made it all you gotta do it create a pull request in the /commands folder here:
+https://github.com/Febzey/ForestBot/tree/main/src/commands
